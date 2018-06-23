70d64ff77acfa717a845e862603aba69df8c2605
LeeBoy <jobboy19890101@gmail.com>
2018-06-23 14:32:00 +0800

    feat: init Project

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'studio_controller'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
